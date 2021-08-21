76c356faf253f1c46953d40d59723fea127727f0
1alessandro1 <46293832+1alessandro1@users.noreply.github.com>
2021-08-21 14:24:42 +0000

    Update setup_samba.sh

--- setup_samba.sh
+++ setup_samba.sh
@@ -1,3 +1,5 @@
+## Dumb script to create quick SMB Server with default unsafe settings - do not trust this SMB.conf file for critical purposes
+
 echo "Installing samba and cifs-utils"
 sudo apt install samba cifs-utils;
 echo "Moving the default config to smb.conf.old"
@@ -13,4 +15,5 @@ sudo mv /home/user/Desktop/smb.conf /etc/samba/smb.conf
 echo "Restarting service..." && systemctl restart smbd
 echo "Change SMB login password for user"
 sudo smbpasswd -a user;
+echo "Showing my IP addresses:"
 ifconfig | grep inet
